e645131c6b142abf01d559f5997b8e155e828ea4
shomix <100691266+shomix@users.noreply.github.com>
2023-05-22 13:16:47 +0200

    v1.0.10

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.Run(async (context) =>
   {
-      await context.Response.WriteAsync("Hello from version v1.0.9");
+      await context.Response.WriteAsync("Hello from version v1.0.10");
   });
 app.Run();
